dba10297ad612d7c2ea9f26c1ecdbb32fb54c6d2
Adam Beagle <abeagle88@gmail.com>
2015-03-04 21:10:23 -0800

    Update <code> css

--- portfolio/manage.py
+++ portfolio/manage.py
@@ -3,9 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # Make sure default setting is production if in production!
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.production")
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.local")
 
     from django.core.management import execute_from_command_line
 
