0ea7fca4c0b24ea77a471d3149e947af66ccdff9
Adam Beagle <abeagle88@gmail.com>
2015-03-05 17:25:16 -0500

    Correct ALLOWED_HOSTS in production settings

--- portfolio/portfolio/settings/production.py
+++ portfolio/portfolio/settings/production.py
@@ -2,6 +2,6 @@ from .base import *
 
 STATIC_URL = 'http://adambeagle.com/static/'
 STATIC_ROOT = BASE_DIR.parent.child('staticfiles')
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.adambeagle.com']
 DEBUG = False
 TEMPLATE_DEBUG = False
