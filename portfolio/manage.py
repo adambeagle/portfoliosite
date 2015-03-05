39fe65eb41a73d7fe4c886437a8e0357f5137bef
Adam Beagle <abeagle88@gmail.com>
2015-03-04 19:17:04 -0800

    Changed default setting to production

--- portfolio/manage.py
+++ portfolio/manage.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     # Make sure default setting is production if in production!
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.production")
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.production")
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portfolio.settings.local")
 
     from django.core.management import execute_from_command_line
 
