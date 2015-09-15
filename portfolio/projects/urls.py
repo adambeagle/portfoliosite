a26246c9fd08b3b35b4ad6fff98fe54ce1460d93
Adam Beagle <abeagle88@gmail.com>
2015-09-15 16:14:12 -0700

    [BLOG BROKEN] Landing page remade, working. templates/base.html, bootstrap-custom.css rebuilt

--- portfolio/projects/urls.py
+++ portfolio/projects/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
-from .views import ProjectIndexView
-
 urlpatterns = patterns('',
-    url(r'^$', ProjectIndexView.as_view(), name="index"),
     url(r'^play-countdown/$', TemplateView.as_view(template_name='projects/countdown.html'), name='countdown'),
 )
