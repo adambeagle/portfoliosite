071f9be5364ce53a8dd1881af2a75a45afc6b6a5
Adam Beagle <abeagle88@gmail.com>
2016-07-04 20:40:39 -0700

    Add swscroll demo and information page.

--- portfolio/projects/urls.py
+++ portfolio/projects/urls.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^countdown-demo/$', TemplateView.as_view(template_name='projects/countdown.html'), name='countdown'),
+    url(r'^swscroll-demo/$', TemplateView.as_view(template_name='projects/swscroll.html'), name='swscroll'),
+    url(r'^swscroll-info/$', TemplateView.as_view(template_name='projects/swscroll-info.html'), name='swscroll-info'),
 )
